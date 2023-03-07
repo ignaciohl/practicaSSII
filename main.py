638d5a6492e8498d7e3f611f6d7faadc10a811ca
marinaparram <mapamarina@gmail.com>
2023-03-07 17:55:22 +0100

    Prueba2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hola, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Holhtyda, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
