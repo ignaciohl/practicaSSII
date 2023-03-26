e2bb19c68e78729ceff57cc17f79067c4943ae2e
NicoleReyesRiv <100412750+NicoleReyesRiv@users.noreply.github.com>
2023-03-26 16:49:30 +0200

    datos devices + numDispositivos bien

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 #crear la bd (tablas)
-con= sqlite3.connect('')
+con= sqlite3.connect('practica.db')
 
 #leer datos proporcionados
 alertas= pd.read_csv('alerts.csv')
@@ -18,11 +18,16 @@ dispositivos= json.load(d)
 
 
 cur=con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS devices(id TEXT, ip TEXT, localizacion TEXT,responsable_id TEXT, analisis_id TEXT)")
-cur.execute("CREATE TABLE IF NOT EXISTS alerts(timestamp TEXT, sid INTEGER, msg TEXT,clasificacion TEXT, prioridad INTEGER, protocolo TEXT, origen INTEGER, destino INTEGER, puerto INTEGER  )")
+
+
+
 cur.execute("CREATE TABLE IF NOT EXISTS responsable(nombre TEXT PRIMARY_KEY, telefono TEXT, rol TEXT)")
 cur.execute("CREATE TABLE IF NOT EXISTS analisis(id INTEGER PRIMARY_KEY, puertos_abiertos TEXT, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS devices(id TEXT, ip TEXT, localizacion TEXT,responsable_id TEXT, analisis_id INTEGER, FOREIGN KEY(responsable_id) REFERENCES responsable(nombre), FOREIGN KEY(analisis_id) REFERENCES analisis(id))")
+cur.execute("CREATE TABLE IF NOT EXISTS alerts(timestamp TEXT, sid INTEGER, msg TEXT,clasificacion TEXT, prioridad INTEGER, protocolo TEXT, origen INTEGER, destino INTEGER, puerto INTEGER  )")
 
+
+## datos tabla responsable
 cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracion de sistemas')")
 cur.execute("INSERT INTO responsable VALUES ('Paco Garcia', '640220120','Direccion')")
 cur.execute("INSERT INTO responsable VALUES ('Luis Sanchez', 'None','Desarrollador')")
@@ -30,6 +35,24 @@ cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracio
 cur.execute("INSERT INTO responsable VALUES ('admiin', 'None','None')")
 cur.execute("INSERT INTO responsable VALUES ('admin', '656445552','Administracion de sistemas')")
 cur.execute("INSERT INTO responsable VALUES ('admin','656445552','Administracion de sistemas')")
+
+# datos tabla analisis
+cur.execute("INSERT INTO analisis VALUES(1, '80/TCP, 443/TCP, 3306/TCP, 40000/UDP', 3, 0, 15)")
+cur.execute("INSERT INTO analisis VALUES(2, 'None', 0, 0, 4)")
+cur.execute("INSERT INTO analisis VALUES(3, '1194/UDP, 8080/TCP,8080/UDP, 40000/UDP', 1, 1, 52)")
+cur.execute("INSERT INTO analisis VALUES(4, '443/UDP, 80/TCP', 1, 0,3)")
+cur.execute("INSERT INTO analisis VALUES(5, '80/TCP, 67/UDP, 68/UDP', 2, 2, 12)")
+cur.execute("INSERT INTO analisis VALUES(6, '8080/TCP, 3306/TCP, 3306/UDP', 2, 0, 2)")
+cur.execute("INSERT INTO analisis VALUES(7, '80/TCP, 443/TCP, 9200/TCP, 9300/TCP, 5601/TCP', 3, 2, 21)")
+
+# datos tabla devices
+cur.execute("INSERT INTO devices VALUES('web', '172.18.0.0', 'None','admin', 1)")
+cur.execute("INSERT INTO devices VALUES('paco_pc', '172.17.0.0', 'Barcelona','Paco Garcia', 2)")
+cur.execute("INSERT INTO devices VALUES('luis_pc', '172.19.0.0', 'Madrid','Luis Sanchez', 3)")
+cur.execute("INSERT INTO devices VALUES('router1', '172.1.0.0', 'None','admin', 4)")
+cur.execute("INSERT INTO devices VALUES('dhcp_server', '172.1.0.1', 'Madrid','admiin', 5)")
+cur.execute("INSERT INTO devices VALUES('mysql_db', '172.18.0.1', 'None','admin', 6)")
+cur.execute("INSERT INTO devices VALUES('ELK', '172.18.0.2', 'None','admin', 7)")
 
 
 
@@ -43,6 +66,10 @@ con.commit()
 df_dispositivos = pd.read_sql_query("SELECT * from devices", con)
 df_analisis=pd.read_sql_query("SELECT * FROM analisis",con)
 numDispositivos = df_dispositivos['id'].nunique()
+print(str(numDispositivos))
+## hasta aquí bien, numDispositivos 7
+
+
 numAlertas=alertas['sid'].nunique()
 mediaPuertos=df_analisis['puertos_abiertos'].mean()
 desvPuertos=df_analisis['puertos_abiertos'].std()
@@ -54,7 +81,7 @@ minPuertos=df_analisis['puertos_abiertos'].min()
 maxPuertos=df_analisis['puertos_abiertos'].max()
 minVulner=df_analisis['vulnerabilidades_detectadas'].min()
 maxVulner=df_analisis['vulnerabilidades_detectadas'].max()
-print(numDispositivos)
+print(str(numDispositivos))
 print(numAlertas)
 print(mediaPuertos, desvPuertos)
 print(mediaServicios, desvServicios)
